95edbc0cd625370cc980c61cc337f1f8b74e6ad5
itsabdullah2 <abdullahosman3020@gmail.com>
2024-12-15 19:33:25 +0200

    all solutions are done :)

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const objs = [
-  { id: 1, name: 'abdullah', address: 'cairo' },
-  { id: 2, name: 'abdullah', address: 'giza' },
-];
 
-objs.map((obj) => console.log(obj));
-for (let obj of objs) {
-  console.log(obj);
-}
